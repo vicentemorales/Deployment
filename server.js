5f50bee398ec985339f073058c75e92e27fe1220
vicentemorales <vmorales121@gmail.com>
2023-05-14 11:27:43 -0700

    config for vercel

--- server.js
+++ server.js
@@ -2,15 +2,20 @@
 //call express
 const express = require('express')
 const app = express()
+require('dotenv').config()
+const PORT = process.env.PORT
 
 
+//get
 app.get('/', (req, res) => {
-  res.send('Hello, world!')
-})
+    res.send('Hello, world!')
+  })
+  
+  app.listen(PORT, () => console.log(`Server is running in port ${PORT}`))
+  
 
 //listen
 app.listen(3000)
 
 //export
 module.export = app;
-
